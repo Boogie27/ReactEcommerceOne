8dc1d568ace304c1024de22a123c4d1284f43283
boogie27 <anonyecharles@gmail.com>
2022-09-08 12:15:52 +0100

    ecommerce-one

--- routes/getUserRoute.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express')
-// const router = express.Router()
-// const { getUser } = require('../controllers/getUserController')
-
-// router.route('/').post(getUser)
-
-
-
-// module.exports = router
